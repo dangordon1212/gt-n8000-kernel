9d525e112d318eeb8fbe91a8700596fc0e7a1d56
Frederic Weisbecker <fweisbec@gmail.com>
2011-11-17 18:48:14 +0100

    nohz: Remove tick_nohz_idle_enter_norcu() / tick_nohz_idle_exit_norcu()
    Those two APIs were provided to optimize the calls of
    tick_nohz_idle_enter() and rcu_idle_enter() into a single
    irq disabled section. This way no interrupt happening in-between would
    needlessly process any RCU job.
    
    Now we are talking about an optimization for which benefits
    have yet to be measured. Let's start simple and completely decouple
    idle rcu and dyntick idle logics to simplify.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    (cherry picked from commit 1268fbc746ea1cd279886a740dcbad4ba5232225)
    
    Conflicts:
    	arch/arm/kernel/process.c
    	arch/openrisc/kernel/idle.c
    	arch/powerpc/kernel/idle.c
    	arch/x86/kernel/process_32.c

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -98,7 +98,8 @@ void cpu_idle(void)
 
 	/* endless idle loop with no priority at all */
 	while (1) {
-		tick_nohz_idle_enter_norcu();
+		tick_nohz_idle_enter();
+		rcu_idle_enter();
 		idle_notifier_call_chain(IDLE_START);
 		while (!need_resched()) {
 
@@ -116,7 +117,8 @@ void cpu_idle(void)
 			start_critical_timings();
 		}
 		idle_notifier_call_chain(IDLE_END);
-		tick_nohz_idle_exit_norcu();
+		rcu_idle_exit();
+		tick_nohz_idle_exit();
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();
