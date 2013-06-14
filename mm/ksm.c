26dbc06f42658e07c12d1903ad2cb2339c538bd3
Kenneth R. Crudup <kcrudup@lytro.com>
2013-06-14 04:35:28 -0700

    mm: use new kthread_freezable_should_stop() in thread
    See commit 9bc7ea9b ("freezer: implement and use
    kthread_freezable_should_stop()")

--- mm/ksm.c
+++ mm/ksm.c
@@ -1446,7 +1446,7 @@ static int ksm_scan_thread(void *nothing)
 				msecs_to_jiffies(ksm_thread_sleep_millisecs));
 		} else {
 			wait_event_freezable(ksm_thread_wait,
-				ksmd_should_run() || kthread_should_stop());
+				ksmd_should_run() || kthread_freezable_should_stop(NULL));
 		}
 	}
 	return 0;
